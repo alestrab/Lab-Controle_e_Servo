e0d8b19ff39331342c7473524b29eda58b07d6df
alestrab <astrabello@estudante.ufscar.br>
2023-05-22 19:06:46 -0300

    Aumento das Fontes dos Gráficos

--- Prática 1/Programas/oscilador_harmonico.m
+++ Prática 1/Programas/oscilador_harmonico.m
@@ -9,21 +9,27 @@ k = out.simout(:,4);
 subplot(2,2,1)
 plot(x,k,'r',x,u,'b','LineWidth',2)
 legend(' Cinética',' Potencial','FontSize',15)
-xlabel('Deslocamento [m]')
-ylabel('Energia [J]')
+xlabel('Deslocamento [m]','FontSize',20)
+ylabel('Energia [J]','FontSize',20)
+ax = gca;
+ax.FontSize = 15;
 grid on
 
 %Energia em função da velocidade
 subplot(2,2,3)
 plot(v,k,'r',v,u,'b','LineWidth',2)
 legend(' Cinética',' Potencial','FontSize',15)
-xlabel(' Velocidade [m/s]')
-ylabel('Energia [J]')
+xlabel(' Velocidade [m/s]','FontSize',20)
+ylabel('Energia [J]','FontSize',20)
+ax = gca;
+ax.FontSize = 15;
 grid on
 
 %Velocidade em função do deslocamento
 subplot(1,2,2)
 plot(x,v,'r','LineWidth',2)
-xlabel('Deslocamento [m]')
-ylabel('Velocidade [m/s]')
+xlabel('Deslocamento [m]','FontSize',20)
+ylabel('Velocidade [m/s]','FontSize',20)
+ax = gca;
+ax.FontSize = 15;
 grid on
